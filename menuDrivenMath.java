f64fbdf452ecc64181374ae28a320d72893199e2
lavanyatheepalapudi <lavanyatheepala@gmail.com>
2025-07-22 20:57:49 +0530

    Adding Assignment1

--- menuDrivenMath.java
+++ menuDrivenMath.java
@@ -4,6 +4,7 @@ public class menuDrivenMath {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
+        char c;
 
         do {
             System.out.println("********** MENU **********");
@@ -17,68 +18,76 @@ public class menuDrivenMath {
             choice = sc.nextInt();
 
             switch (choice) {
-                case 1: {
+                case 1: 
                     System.out.print("Enter a number to print its table: ");
-                    int n = sc.nextInt();
-                    System.out.println("Table of " + n + ":");
+                    int n1 = sc.nextInt();
+                    System.out.println("Table of " + n1 + ":");
                     for (int i = 1; i <= 10; i++) {
-                        System.out.println(n + " * " + i + " = " + (n * i));
+                        System.out.println(n1 + " * " + i + " = " + (n1 * i));
                     }
                     break;
-                }
 
-                case 2: {
+                case 2: 
                     System.out.print("Enter a number to calculate factorial: ");
-                    int n = sc.nextInt();
+                    int n2 = sc.nextInt();
                     int fact = 1;
-                    for (int i = 1; i <= n; i++) {
+                    for (int i = 1; i <= n2; i++) {
                         fact *= i;
                     }
-                    System.out.println("Factorial of " + n + " is: " + fact);
+                    System.out.println("Factorial of " + n2 + " is: " + fact);
                     break;
-                }
 
-                case 3: {
+                case 3: 
                     System.out.print("Enter a number to check if it's prime: ");
-                    int n = sc.nextInt();
-                    int count = 0;
-                    for (int i = 1; i <= n; i++) {
-                        if (n % i == 0) {
-                            count++;
+                    int n3 = sc.nextInt();
+                    boolean isPrime = true;
+
+                    if (n3 <= 1) {
+                        isPrime = false;
+                    } else {
+                        for (int i = 2; i <= n3 / 2; i++) {
+                            if (n3 % i == 0) {
+                                isPrime = false;
+                                break;
+                            }
                         }
                     }
-                    if (count == 2)
-                        System.out.println(n + " is a prime number.");
-                    else
-                        System.out.println(n + " is not a prime number.");
-                    break;
-                }
 
-                case 4: {
+                    if (isPrime) {
+                        System.out.println(n3 + " is a prime number.");
+                    } else {
+                        System.out.println(n3 + " is not a prime number.");
+                    }
+                    break;
+
+                case 4: 
                     System.out.println("First 10 numbers of the Fibonacci series:");
-                    int a = 0, b = 1, c;
+                    int a = 0, b = 1;
                     System.out.print(a + " " + b + " ");
                     for (int i = 3; i <= 10; i++) {
-                        c = a + b;
-                        System.out.print(c + " ");
+                        int c1 = a + b;
+                        System.out.print(c1 + " ");
                         a = b;
-                        b = c;
+                        b = c1;
                     }
                     System.out.println(); 
                     break;
-                }
 
                 case 5:
-                    System.out.println("Exit");
+                    System.out.println("Exit.");
                     break;
 
                 default:
                     System.out.println("Invalid choice! Please enter a number between 1 and 5.");
             }
 
-            System.out.println(); 
-        } while (choice != 5);
+            System.out.print("\nBack to Menu? (Y/N): ");
+            c = sc.next().charAt(0);
 
-        sc.close();
+        } while (c == 'Y' || c== 'y');
+
+        
+
+        
     }
 }
